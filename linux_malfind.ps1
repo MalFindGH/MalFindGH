e6e5fe22454cfa062365000781630fd37d170602
MalFindGH <138940598+MalFindGH@users.noreply.github.com>
2023-07-13 19:20:51 -0700

    Create linux_malfind.ps1

---
+++ linux_malfind.ps1
@@ -0,0 +1,23 @@
+# WARNINGS:
+# This script may download actual linux malware such as Mozi or Mirai. These malware are botnets, so take caution to prevent infection of your computer
+# by Mozi or Mirai or any other botnet for that matter. Please run this script in a safe environment to ensure proper execution and prevention of infection
+# by malware.
+
+
+# Generate a random IP address
+$ipAddress = '{0}.{1}.{2}.{3}' -f (Get-Random -Minimum 1 -Maximum 256), (Get-Random -Minimum 1 -Maximum 256), (Get-Random -Minimum 1 -Maximum 256), (Get-Random -Minimum 1 -Maximum 256)
+$testUrl = "http://$ipAddress"
+
+# Attempt to download the file from the generated IP address
+# try {
+    Write-Host "Connecting... : $ipAddress"
+    Invoke-WebRequest -Uri "$testUrl/i" -OutFile "C:\AddrFinder" -Verbose
+    Invoke-WebRequest -Uri "$testUrl/mozi.a" -OutFile "C:\AddrFinder" -Verbose
+    Invoke-WebRequest -Uri "$testUrl/.i" -OutFile "C:\AddrFinder" -Verbose
+    Invoke-WebRequest -Uri "$testUrl/bin.sh" -OutFile "C:\AddrFinder" -Verbose
+    Invoke-WebRequest -Uri "$testUrl/mozi.m" -OutFile "C:\AddrFinder" -Verbose
+
+# } catch {
+    # Write-Host "Failed to download the file from IP address: $ipAddress"
+    # Write-Host "Error message: $($_.Exception.Message)"
+# }
